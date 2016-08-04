f1624f15438d717e9b1b28742c941cde6cd13fb8
Ulysses <ulyssesr@yahoo.com>
2016-08-04 10:36:08 -0700

    change file names

--- ur_main.php
+++ ur_main.php
@@ -1,13 +1,13 @@
 <?php
  
-if($_POST['urr_webfont_hidden'] == 'Y') {
+if($_POST['ur_webfont_hidden'] == 'Y') {
 
   // store data
 
-  $name = $_POST['urr_webfont_name'];
-  update_option('urr_webfont_name', $name);
-  $style = $_POST['urr_webfont_style'];
-  update_option('urr_webfont_style', $style);
+  $name = $_POST['ur_webfont_name'];
+  update_option('ur_webfont_name', $name);
+  $style = $_POST['ur_webfont_style'];
+  update_option('ur_webfont_style', $style);
 
   ?>
   <div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>
@@ -16,25 +16,25 @@ if($_POST['urr_webfont_hidden'] == 'Y') {
 } else {
 
   //Normal page display
-  $name = get_option('urr_webfont_name');
-  $style = get_option('urr_webfont_style');
+  $name = get_option('ur_webfont_name');
+  $style = get_option('ur_webfont_style');
 }
 
 ?>
 
 <div class="wrap">
 
-  <?php echo "<h2>" . __( 'UR Web Fonts Setting', 'urr_webfont_setting' ) . "</h2>"; ?>
+  <?php echo "<h2>" . __( 'UR Web Fonts Setting', 'ur_webfont_setting' ) . "</h2>"; ?>
 
-  <form name="urr_webfont_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
-  <input type="hidden" name="urr_webfont_hidden" value="Y">
+  <form name="ur_webfont_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+  <input type="hidden" name="ur_webfont_hidden" value="Y">
 
   <h3><?php _e("Add Google Web Font Stylesheet Link: " ); ?></h3>
 
   <p>
   Example:<br/>
   <code><?php echo "&#60;link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'&#62;";?></code><br/><br/>
-  <textarea name="urr_webfont_name" rows="5" cols="150"><?php echo stripslashes($name); ?></textarea>
+  <textarea name="ur_webfont_name" rows="5" cols="150"><?php echo stripslashes($name); ?></textarea>
   </p>
 
   <h3><?php _e("Add Your Own Custom Stylesheet: " ); ?></h3>
@@ -51,7 +51,7 @@ if($_POST['urr_webfont_hidden'] == 'Y') {
   <hr />
 
   <p class="submit">
-  <input type="submit" name="Submit" value="<?php _e('Update Options', 'urr_google_webfont' ) ?>" />
+  <input type="submit" name="Submit" value="<?php _e('Update Options', 'ur_google_webfont' ) ?>" />
   </p>
 
   </form>
--- ur_web_fonts.php
+++ ur_web_fonts.php
@@ -3,24 +3,24 @@
 /*
 Plugin Name: UR Web Fonts
 Plugin URI: http://uly.me/google-web-fonts-plugin/
-Description: A WordPress Plugin That Utilizes Google Webfonts.
-Version: 1.2
+Description: A WordPress Plugin That Utilizes Google Web Fonts.
+Version: 1.3
 Author: Ulysses Ronquillo
 Author URI: http://uly.me
 */
 
 // include main file
 
-function urr_google_webfonts() {
-	include('urr_main.php');
+function ur_google_webfonts() {
+	include('ur_main.php');
 }
 
 // add this plugin under the appearance theme pages
 
-add_action( 'admin_menu', 'urr_google_webfonts_add_options' );
+add_action( 'admin_menu', 'ur_google_webfonts_add_options' );
 
-function urr_google_webfonts_add_options() {
-	add_theme_page( 'UR Web Fonts', 'UR Web Fonts', 'manage_options', __FILE__, 'urr_google_webfonts' );
+function ur_google_webfonts_add_options() {
+	add_theme_page( 'UR Web Fonts', 'UR Web Fonts', 'manage_options', __FILE__, 'ur_google_webfonts' );
 }
 
 // add this plugin in the admin bar menu
@@ -32,7 +32,7 @@ function add_toolbar_items($admin_bar){
 	$admin_bar->add_menu( array(
 		'id'    => 'ur-web-fonts',
 		'title' => 'UR Web Fonts',
-		'href'  => admin_url('options-general.php?page=webfonts/urwebfonts.php'),	
+		'href'  => admin_url('options-general.php?page=webfonts/ur_web_fonts.php'),	
 		'meta'  => array(
 		'title' => __('UR Web Fonts'),			
 		),
@@ -42,15 +42,15 @@ function add_toolbar_items($admin_bar){
 
 // add code to the wp_head section
 
-function urr_webfonts_header() {
+function ur_webfonts_header() {
 	//get options
-	$name = get_option('urr_webfont_name');
-	$style = get_option('urr_webfont_style');
+	$name = get_option('ur_webfont_name');
+	$style = get_option('ur_webfont_style');
 	// display to wp_head
 	echo stripslashes($name);
 	echo '<style type="text/css">'.stripslashes($style).'</style>';
 }
 
-add_action( 'wp_head', 'urr_webfonts_header' );
+add_action( 'wp_head', 'ur_webfonts_header' );
 
 /* end of webfonts.php */
